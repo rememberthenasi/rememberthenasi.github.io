9f2891d7c93e8482ec2f9fc9c7e470c23bf4e34b
Menachem Spielman <116475389+notapelican@users.noreply.github.com>
2025-03-02 22:08:40 +0000

    add webapp option

---
+++ service-worker.js
@@ -0,0 +1,27 @@
+const CACHE_NAME = 'prayer-display-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/styles.css',
+  '/script.js',
+  // Add other assets you want to cache
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
+  );
+});
